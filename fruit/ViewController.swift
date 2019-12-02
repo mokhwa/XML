aad3dc03407e511ad637af26831a91cb9e2300e1
ybh <2616442@naver.com>
2019-12-02 16:42:44 +0900

    jumo

--- fruit/ViewController.swift
+++ fruit/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, XMLParserDelegate {
+    var item:[String:String] = [:]
+    var elements:[[String:String]] = []
+    var currentElement = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.url(forResource: "Fruit", withExtension: "XML")
+        if path != nil {
+            let myparser = XMLParser(contentsOf: path!)
+            if XMLParser(contentsOf: path!) != nil {
+                myparser?.delegate = self
+                if(myparser?.parse())! {
+                    print("succuess")
+                    print(elements)
+                }
+                else{
+                    print("error")
+                }
+            }
+            else {
+                print("error")
+            }
+        }
     }
 
-
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        currentElement = elementName
+        print(currentElement)
+    }
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        print("data = \(data)")
+        if !data.isEmpty {
+            item[currentElement] = data
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            elements.append(item)
+        }
+    }
 }
-
